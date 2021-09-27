ba05ca4ef85e8b5ed3a515983c6c38345d6160c0
Gott50 <xxGott50xx@gmail.com>
2021-09-27 20:21:42 +0200

    run prettier

--- yourtrading-ai/src/index.tsx
+++ yourtrading-ai/src/index.tsx
@@ -3,16 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Amplify from "aws-amplify";
-import awsExports from "./aws-exports";
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
