5aa76ce4720ddc260de063ace06cc8f9784c0aa9
Gott50 <xxGott50xx@gmail.com>
2021-10-15 22:43:56 +0200

    Updates

---
+++ static/js/17.cfd98b3f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpyourtrading-ai"]=this["webpackJsonpyourtrading-ai"]||[]).push([[17],{567:function(t,n,e){"use strict";e.r(n),e.d(n,"amplify_auth_container",(function(){return r}));var i=e(35),r=function(){function t(t){Object(i.k)(this,t)}return t.prototype.render=function(){return Object(i.i)(i.b,null,Object(i.i)("form",{autoComplete:"on",hidden:!0},Object(i.i)("input",{name:"username"}),Object(i.i)("input",{name:"password",type:"password"}),Object(i.i)("input",{type:"submit"})),Object(i.i)("slot",null))},t}()}}]);
+//# sourceMappingURL=17.cfd98b3f.chunk.js.map
